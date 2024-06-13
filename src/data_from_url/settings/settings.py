2a9370d9dc9ba435b5837521a795a5c85b648154
Maarten de Ruyter <devops@geodatagarden.com>
2024-06-13 23:21:03 +0200

    update default headers

--- src/data_from_url/settings/settings.py
+++ src/data_from_url/settings/settings.py
@@ -5,11 +5,7 @@ Module to define constants for the get_data module
 # default headed will be used when no header os provided in the request_params
 # for the GetData.result method
 DEFAULT_HEADER = {
-    "User-Agent": (
-        "Mozilla/5.0 (Windows NT 6.1; WOW64) "
-        "AppleWebKit/537.36 (KHTML, like Gecko) "
-        "Chrome/56.0.2924.76 Safari/537.36"
-    ),
+    "User-Agent": 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/102.0.5005.61 Safari/537.36',
     "accept": "json/application",
 }
 
